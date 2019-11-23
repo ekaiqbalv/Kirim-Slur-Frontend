e4ed900f5a057a3d4586cc16032166f7a15f4777
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2019-11-23 22:06:50 +0700

    CRUD Pelanggan Selesai

--- www/js/pelanggan.js
+++ www/js/pelanggan.js
@@ -3,13 +3,70 @@ var Application = {
     $(window).load("pageinit", "#list-data", function() {
       Application.getListPelanggan();
     });
-    $(document).on("click", "#detail-pelanggan", function() {
-      let id = $(this).data("idpelanggan");
-      Application.getPelangganById(id);
+    $(document).on("click", "#btn-tambah-pelanggan", function() {
+      let inputanKosong = "false";
+      let inputan = $("#addDataPelanggan").find("input");
+      inputan.each(function() {
+        if ($(this).val() == "") {
+          inputanKosong = "true";
+        }
+      });
+      if (inputanKosong == "false") {
+        let dataForm = $("#addDataPelanggan").serialize();
+        Application.addPelanggan(dataForm);
+      }
     });
-    $(document).on("click", "#delete-pelanggan", function() {
+    $(document).on("click", "#detail-pelanggan", async function() {
+      let id = $(this).data("idpelanggan");
+      let dataPelanggan = await Application.getPelangganById(id);
+      window.location.href = "#detail-data";
+      $("#p-nama").html(dataPelanggan.data.nama);
+      $("#p-alamat").html(dataPelanggan.data.alamat);
+      $("#p-kota").html(dataPelanggan.data.kota);
+      $("#p-kode-pos").html(dataPelanggan.data.kodepos);
+      $("#p-no-telp").html(dataPelanggan.data.no_telp);
+      $("#edit-pelanggan").data("idpelanggan", dataPelanggan.data.id);
+      $("#btn-hapus-pelanggan").data("idpelanggan", dataPelanggan.data.id);
+    });
+    $(document).on("click", "#edit-pelanggan", async function() {
+      let id = $(this).data("idpelanggan");
+      let dataPelanggan = await Application.getPelangganById(id);
+      window.location.href = "#edit-data";
+      $("#input-nama").val(dataPelanggan.data.nama);
+      $("#input-alamat").val(dataPelanggan.data.alamat);
+      $("#input-kota").val(dataPelanggan.data.kota);
+      $("#input-kode-pos").val(dataPelanggan.data.kodepos);
+      $("#input-no-telp").val(dataPelanggan.data.no_telp);
+      $("#btn-edit-pelanggan").data("idpelanggan", dataPelanggan.data.id);
+    });
+    $(document).on("click", "#btn-edit-pelanggan", function() {
+      let id = $(this).data("idpelanggan");
+      let dataForm = $("#editDataPelanggan").serialize();
+      Application.updatePelanggan(id, dataForm);
+    });
+    $(document).on("click", "#btn-hapus-pelanggan", function() {
       let id = $(this).data("idpelanggan");
       Application.deletePelanggan(id);
+    });
+  },
+
+  addPelanggan: function(dataForm) {
+    $.ajax({
+      url: "http://kirimslur-server.herokuapp.com/pelanggan/",
+      type: "post",
+      data: dataForm,
+      beforeSend: function() {
+        $.mobile.loading("show", {
+          text: "Sedang menyimpan data pelanggan...",
+          textVisible: true
+        });
+      },
+      success: function(dataObject) {
+        window.location.replace("pelanggan.html");
+      },
+      complete: function(dataObject) {
+        $.mobile.loading("hide");
+      }
     });
   },
 
@@ -19,16 +76,14 @@ var Application = {
       type: "get",
       beforeSend: function() {
         $.mobile.loading("show", {
-          text: "Please wait while retrieving data...",
+          text: "Sedang mengambill data...",
           textVisible: true
         });
       },
       success: function(dataObject) {
         for (let index = 0; index < dataObject.data.length; index++) {
           var listPelanggan =
-            '<li><a href="#detail-data?id=' +
-            dataObject.data[index].id +
-            '" target="_self" id="detail-pelanggan" data-idpelanggan="' +
+            '<li><a target="_self" id="detail-pelanggan" data-idpelanggan="' +
             dataObject.data[index].id +
             '"><h2>' +
             dataObject.data[index].nama +
@@ -47,36 +102,63 @@ var Application = {
     });
   },
 
-  getPelangganById: function(id) {
-    $.ajax({
+  getPelangganById: async function(id) {
+    let dataPelanggan;
+    await $.ajax({
       url: "http://kirimslur-server.herokuapp.com/pelanggan/" + id,
       type: "get",
       beforeSend: function() {
         $.mobile.loading("show", {
-          text: "Please wait while retrieving data...",
+          text: "Sedang mengambill data...",
           textVisible: true
         });
       },
       success: function(dataObject) {
-        $("#p-nama").html(dataObject.data.nama);
-        $("#p-alamat").html(dataObject.data.alamat);
-        $("#p-kota").html(dataObject.data.kota);
-        $("#p-kode-pos").html(dataObject.data.kodepos);
-        $("#p-no-telp").html(dataObject.data.no_telp);
-        $("#delete-pelanggan").data("idpelanggan", dataObject.data.id);
+        dataPelanggan = dataObject;
+      },
+      complete: function(dataObject) {
+        $.mobile.loading("hide");
+      }
+    });
+    return dataPelanggan;
+  },
+
+  updatePelanggan: function(id, dataForm) {
+    $.ajax({
+      url: "http://kirimslur-server.herokuapp.com/pelanggan/" + id,
+      type: "put",
+      data: dataForm,
+      beforeSend: function() {
+        $.mobile.loading("show", {
+          text: "Sedang menyimpan data pelanggan...",
+          textVisible: true
+        });
+      },
+      success: function(dataObject) {
+        window.location.replace("pelanggan.html");
       },
       complete: function(dataObject) {
         $.mobile.loading("hide");
       }
     });
   },
+
   deletePelanggan: function(id) {
     $.ajax({
       url: "http://kirimslur-server.herokuapp.com/pelanggan/" + id,
       type: "delete",
+      beforeSend: function() {
+        $.mobile.loading("show", {
+          text: "Menghapus data pelanggan...",
+          textVisible: true
+        });
+      },
       success: function(dataObject) {
         window.location.replace("pelanggan.html");
+      },
+      complete: function(dataObject) {
+        $.mobile.loading("hide");
       }
     });
-  },
+  }
 };
